3e2ae17a745e612b1b1aaaeaa26f8731872cb0a6
joslingg <truonghoanglinh787@gmail.com>
2024-07-01 16:29:40 +0700

    *update*

--- quan_ly_thuoc_ksdb/quan_ly_thuoc_ksdb/settings.py
+++ quan_ly_thuoc_ksdb/quan_ly_thuoc_ksdb/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -56,7 +57,9 @@ ROOT_URLCONF = 'quan_ly_thuoc_ksdb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'quan_ly_thuoc_ksdb/templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,8 +119,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-STATIC_URL = 'static/'
+STATIC_URL = 'quan_ly_thuoc_ksdb/static/'
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, "quan_ly_thuoc_ksdb/static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
